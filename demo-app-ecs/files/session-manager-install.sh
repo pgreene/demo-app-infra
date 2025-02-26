939d3afc221e1f82c29c9ad1bde7fbf2e8fd1c2e
pgreene <pgreene@protonmail.com>
2025-02-26 16:48:36 -0500

    removed file

--- demo-app-ecs/files/session-manager-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-unzip sessionmanager-bundle.zip
-sudo ./sessionmanager-bundle/install -i /usr/local/sessionmanagerplugin -b /usr/local/bin/session-manager-plugin
-./sessionmanager-bundle/install -h
-session-manager-plugin
-session-manager-plugin --version
